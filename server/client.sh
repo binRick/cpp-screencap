47c1bf4cda49e032a87866b62a09316376f1a81d
rick <rick@ricks-MBP.lan>
2023-10-12 17:58:37 -0400

    add flask server app

--- server/client.sh
+++ server/client.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 set -eou pipefail
 JSON="{\"wow\":\"def\"}"
-cmd="curl -H 'Content-Type: application/json' -H 'Accept: application/json' -X POST -d '$JSON' -vk 'http://localhost:5005/data'"
+URL="http://localhost:5005/data"
+FILE="../monitor_0.gif"
+tf="$(mktemp)"
+
+#trap "unlink $tf" EXIT
+
+cmd="jo abc=123"
+eval "$cmd" > $tf
+
+
+cmd="curl -H 'Content-Type: application/json' -H 'Accept: application/json' -X POST -d @$tf -vk '$URL'"
+echo -e "$cmd"
 eval "$cmd"
+#cmd="curl -H 'Content-Type: application/json' -H 'Accept: application/json' -X POST -d '$JSON' -vk '$URL'"
+
+#cmd="curl -X POST -H 'Content-Type: application/json' -sk -F file=@$FILE '$URL'"
+#eval "$cmd"
+
--- server/server.py
+++ server/server.py
@@ -1,12 +1,58 @@
-import os, sys, json
+import os, sys, json, logging
 from flask import Flask, request, jsonify
+from flask import flash, redirect, url_for
+from werkzeug.utils import secure_filename
+from flask import send_from_directory
 
 
+
+UPLOAD_FOLDER = './files'
+ALLOWED_EXTENSIONS = {'gif'}
+
+logging.basicConfig(filename='server.log', encoding='utf-8', level=logging.DEBUG)
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.logger.setLevel(logging.DEBUG)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/uploads/<name>')
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
 
 @app.route('/data', methods=['POST']) 
 def foo():
     data = request.json
     j = jsonify(data)
+    logging.debug(f'req: {data}')
     print(j)
     return j
+
+
+@app.route('/file', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        print(request.files)
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
